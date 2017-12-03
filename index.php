42249440bc5590ade9d3e5f0c314096039355923
mrNoBoDy <mrnobody1042@ya.ru>
2017-12-03 19:44:09 +0300

    Сделано: Добавлены фильтры для выборки, выборка по каждому фильтру суммируется с предыдущей

--- index.php
+++ index.php
@@ -1,10 +1,50 @@
+<h2>Find:</h2>
+<form method="post">
+  <?php
+  // Вывод полей для выборки
+  $cols = array('name', 'author', 'year', 'isbn', 'genre');
+  foreach ($cols as $value) {
+    echo "<p>".ucfirst($value).": </p><input type=\"text\" name=\"$value\">";
+  }
+  ?>
+  <input type="submit" value="Find">
+</form>
+
 <?php
+// Получаем логин и пароль для подключения
 $conf = file_get_contents('config.json');
 $conf = json_decode($conf, true);
-$connection = new PDO("mysql:host=localhost;dbname=global;charset=utf8", $conf['user'], $conf['pass']);
+// Создаем подключение
+$connection = new PDO(
+  "mysql:host=localhost;dbname=global;charset=utf8",
+  $conf['user'],
+  $conf['pass']);
+
+  // Функция для дополнения запроса
+  function BuildQuery($query, $field)
+  {
+    $query .= (strpos($query,'WHERE')) ? 'OR ' : ' WHERE ';
+    $query = $query."$field LIKE '%$_POST[$field]%' ";
+    return $query;
+  }
+
+// Создаем основу запроса
 $query = "SELECT * FROM books";
-$cols = array('id', 'name', 'author', 'year', 'isbn', 'genre');
+// Находим заполненные поля выборки и сохраняем в отд
+foreach ($cols as $value) {
+  $fields = array();
+  if (isset($_POST[$value]) && !empty($_POST[$value])) {
+    $query = BuildQuery($query, $value);
+  }
+}
+
+var_dump($query);
+$select = $connection->prepare($query);
+$select->execute();
+$query = $select->fetchAll();
+
 ?>
+
 <meta charset="utf-8">
 <table border="1">
   <tr>
@@ -12,7 +52,7 @@ $cols = array('id', 'name', 'author', 'year', 'isbn', 'genre');
       echo "<td><strong>$value</strong></td>";
     } ?>
   </tr>
-  <?php foreach ($connection->query($query) as $row) {?>
+  <?php foreach ($query as $row) {?>
     <tr>
       <?php foreach ($cols as $key) {
         echo "<td>$row[$key]</td>";
