3f10636bea0851d1dc06be1951fd228d0ff69325
mrNoBoDy <mrnobody1042@ya.ru>
2017-12-03 19:51:16 +0300

    Добавлено: Введеное значение в поле сохраняется после выполнения запроса, комментарии и форматирование

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   // Вывод полей для выборки
   $cols = array('name', 'author', 'year', 'isbn', 'genre');
   foreach ($cols as $value) {
-    echo "<p>".ucfirst($value).": </p><input type=\"text\" name=\"$value\">";
+    echo "<p>".ucfirst($value).
+    ": </p><input type=\"text\" name=\"$value\" value=\"$_POST[$value]\">";
   }
   ?>
   <input type="submit" value="Find">
@@ -14,6 +15,7 @@
 // Получаем логин и пароль для подключения
 $conf = file_get_contents('config.json');
 $conf = json_decode($conf, true);
+
 // Создаем подключение
 $connection = new PDO(
   "mysql:host=localhost;dbname=global;charset=utf8",
@@ -23,36 +25,40 @@ $connection = new PDO(
   // Функция для дополнения запроса
   function BuildQuery($query, $field)
   {
+    // Если WHERE в запросе уже есть, значит мы добавляем второе условие
+    // и нужен OR
     $query .= (strpos($query,'WHERE')) ? 'OR ' : ' WHERE ';
     $query = $query."$field LIKE '%$_POST[$field]%' ";
     return $query;
   }
 
 // Создаем основу запроса
-$query = "SELECT * FROM books";
-// Находим заполненные поля выборки и сохраняем в отд
+$select = "SELECT * FROM books";
+// Находим заполненные поля выборки
 foreach ($cols as $value) {
   $fields = array();
   if (isset($_POST[$value]) && !empty($_POST[$value])) {
-    $query = BuildQuery($query, $value);
+    $select = BuildQuery($select, $value);
   }
 }
 
-var_dump($query);
-$select = $connection->prepare($query);
+// Подгатавливаем запрос и выполняем его
+$select = $connection->prepare($select);
 $select->execute();
-$query = $select->fetchAll();
-
+// Сохраняем результат запроса
+$result = $select->fetchAll();
 ?>
 
-<meta charset="utf-8">
 <table border="1">
   <tr>
     <?php foreach ($cols as $value) {
+      // Выводим шапку таблицы
       echo "<td><strong>$value</strong></td>";
     } ?>
   </tr>
-  <?php foreach ($query as $row) {?>
+  <?php foreach ($result as $row) {
+    // Выводим строки полученные запросом
+    ?>
     <tr>
       <?php foreach ($cols as $key) {
         echo "<td>$row[$key]</td>";
